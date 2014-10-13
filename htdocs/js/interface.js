aeffead05dff3da83bc83a751d11070e38ada168
Stefan Hammer <s.hammer@univie.ac.at>
2014-10-13 16:04:39 +0200

    redesigned the user interface

--- htdocs/js/interface.js
+++ htdocs/js/interface.js
@@ -52,6 +52,10 @@ function RNAViewModel() {
     return this.input().replace(/^[\r\n]+|[\r\n]+$/g,"").split("\n"); //remove leading/trailing newlines and split in between
   }, this);
   
+  self.addMolecule = function() {
+    $('#add').modal('show');
+  }
+  
   self.submit = function() {
     $('#inputError').hide();
     if (self.ss().length != 2) {
@@ -63,7 +67,7 @@ function RNAViewModel() {
     } else {
       ajax(serverURL + '/struct_graph', 'POST', JSON.stringify( {seq: self.ss()[0], struct: self.ss()[1]} )).success( function(data) {
         self.graph = new Graph(data);
-        $('#output-tab').show();
+        $('#add').modal('hide');
       }).error( function(jqXHR) { 
         showError("Ajax error (" + jqXHR.status + ") Please check your input!", "inputError");
       });
@@ -100,33 +104,6 @@ function RNAViewModel() {
   }
 }
 
-$(document).ready(function() {
-        $("#save_as_svg").click(function() { 
-                console.log("hello"); 
-                var svg = document.getElementById('plotting-area');
-
-                //get svg source.
-                var serializer = new XMLSerializer();
-                var source = serializer.serializeToString(svg);
-
-                //add name spaces.
-                if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
-                    source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
-                    }
-                    if(!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)){
-                        source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
-                        }
-
-                        //add xml declaration
-                        source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-
-                        //convert svg source to URI data scheme.
-                        var url = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
-
-                        //set url value to a element's href attribute.
-                        location.href = url;
-                });
-        });
 // bind the model to the ui
 var ViewModel = new RNAViewModel();
 ko.applyBindings(ViewModel);
