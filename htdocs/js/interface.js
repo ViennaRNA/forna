d074d102247cf6fd662d6903c7a5065c259e56fe
Stefan Hammer <s.hammer@univie.ac.at>
2014-12-05 16:11:29 +0100

    added fullscreen mode

--- htdocs/js/interface.js
+++ htdocs/js/interface.js
@@ -19,6 +19,33 @@ setPlottingArea = function() {
   $("#plotting-area").width(bodywidth);
 }
 
+function toggleFullScreen(id) {
+  var elem = document.getElementById(id);
+  
+  if (!document.fullscreenElement &&    // alternative standard method
+      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
 // custom ajax call
 ajax = function(uri, method, data) {
   var request = {
