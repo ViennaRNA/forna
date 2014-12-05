61374b9e737d2ee2213c95c6e59be2d37de3faea
Stefan Hammer <s.hammer@univie.ac.at>
2014-12-05 16:54:06 +0100

    peter suggested to make a more visible border. there it is. and no more than that...

--- htdocs/js/interface.js
+++ htdocs/js/interface.js
@@ -16,7 +16,7 @@ setPlottingArea = function() {
   var chartheight = $(window).height();
   if (!document.fullscreenElement &&    // alternative standard method
     !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {
-    chartheight = chartheight-120;
+    chartheight = chartheight-124;
   }
   
   $("#plotting-area").height(chartheight);
