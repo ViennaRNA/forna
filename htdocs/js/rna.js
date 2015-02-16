2bb2ba523d6fc33abc1d0c3036dfb595b086046d
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2015-02-16 22:48:11 +0100

    Bug fix and documentation updates

--- htdocs/js/rna.js
+++ htdocs/js/rna.js
@@ -338,7 +338,7 @@ function ColorScheme(colors_text) {
                 curr_molecule = lines[i].trim().slice(1);
                 counter = 1;
 
-                colors_json[curr_molecule] = {};
+                colors_json.color_values[curr_molecule] = {};
                 continue;
             }
 
@@ -569,7 +569,7 @@ function RNAGraph(seq, dotbracket, struct_name) {
         };
 
         for (i=0; i < self.elements.length; i++) {
-            if (self.elements[i][0] == 's')
+            if (self.elements[i][0] == 's' || self.elements[i][0] == 'e')
                 continue;
 
             var nucs = self.elements[i][2].filter(filter_nucs);
